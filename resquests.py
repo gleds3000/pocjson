de46c36e1eab8c7599e54f8fb4d157c3529802e0
Gleds3000 <gleds3000@gmail.com>
2025-03-25 01:40:53 -0300

    segunda parte

---
+++ resquests.py
@@ -0,0 +1,21 @@
+from flask import request
+
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
+
+
+@app.route('/valida', methods=['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)   
